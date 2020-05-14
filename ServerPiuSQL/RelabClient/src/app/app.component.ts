dc136c18e6828a8ff970113ae2f9a08b37c8526d
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-05-14 19:44:41 +0000

    04.1

---
+++ ServerPiuSQL/RelabClient/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { GeoFeatureCollection } from './models/geojson.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'ang-maps';
+  // google maps zoom level
+  geoJsonObject: GeoFeatureCollection; //Oggetto che conterrà il vettore di GeoJson
+  obsGeoData: Observable<GeoFeatureCollection>;
+
+  constructor(public http: HttpClient) {
+  //Facciamo iniettare il modulo HttpClient dal framework Angular (ricordati di importare la libreria)
+  }
+
+  //Metodo che scarica i dati nella variabile geoJsonObject
+  prepareData = (data: GeoFeatureCollection) => {
+    this.geoJsonObject = data
+    console.log( this.geoJsonObject );
+  }
+
+  //Una volta che la pagina web è caricata, viene lanciato il metodo ngOnInit scarico i    dati
+  //dal server
+  ngOnInit() {
+    this.obsGeoData = this.http.get<GeoFeatureCollection>("https://3000-e9ca05e6-0801-4b53-8764-095f3842f6ee.ws-eu01.gitpod.io/");
+    this.obsGeoData.subscribe(this.prepareData);
+  }
+}
