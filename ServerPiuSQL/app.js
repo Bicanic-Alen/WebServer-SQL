833928a0dd091ea984cdbcc57038e2a0e284e579
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-05-29 08:58:37 +0000

    4.1.5

--- ServerPiuSQL/app.js
+++ ServerPiuSQL/app.js
@@ -25,6 +25,14 @@ app.get('/ci_vettore/:foglio', function (req, res) {
     sqlUtils.connect(req, res, sqlUtils.ciVettGeoRequest);
  });
 
+ 
+app.get('/geogeom/:lng/:lat/:r', function (req, res) {
+     //richiamo il metodo che ottiene l'elenco dei vettori energetici
+     console.log(req.params);
+    sqlUtils.connect(req, res, sqlUtils.geoGeomRequest);
+ });
+
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
